f5aed01f3492a67a3770143ba5365ccbb9e0f2be
hyobin <hyobin2364@gmail.com>
2025-09-09 09:27:55 +0900

    0908

---
+++ if_test3.py
@@ -0,0 +1,29 @@
+# 명령어는 모드 실행
+# 조건문을 이용하면 특정 명령문은 실행되지 않게 할 수 있다
+# 조건문은 if 조건 :
+# 들여쓰기를 해서 if에 하위 명령어로 만든다 --> 블락
+age = 20
+if age >= 18:
+    print('성인입니다.')
+    print('조건문은 True 입니다.')
+    #
+    #
+    #
+print('if와 상관 없는 명령어')
+
+#조건에 맞으면 합격 그렇지 않으면 불합격
+score = 50
+if score >= 60:
+    print('합격')
+else:
+    print('불합격')
+
+tem = 25
+if tem > 30:
+    print('덥다')
+elif tem > 20:
+    print('따뜻하다')
+elif tem > 10:
+    print('시원하다')
+else:
+    print('춥다')
